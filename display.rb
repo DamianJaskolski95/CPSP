5cf14667899448aa24c823b894b5a44a904ce476
DamianJaskolski95 <damian.jaskolski95@gmail.com>
2020-01-09 18:37:49 +0100

    fix, adds read option to opening argument file

--- display.rb
+++ display.rb
@@ -4,7 +4,7 @@ if ARGV.empty?
   file = File.open('webserver.log', 'r')
 else
   if File.exist?(ARGV[0])
-    file = File.open(ARGV[0])
+    file = File.open(ARGV[0], 'r')
   else
     puts 'File does not exist'
     return 0
@@ -26,7 +26,7 @@ end
 
 puts "\nSorted array of unique visits"
 sorted_uniq_visits.each do |line|
-  print "#{line.first} -> #{line.last} unique visits\n"
+  print "#{line.first} -> #{line.last} unique    visits\n"
 end
 
 puts "\n"
