e2c76b127d65752c3be3c9584ffc469148c4f0b7
DamianJaskolski95 <damian.jaskolski95@gmail.com>
2020-01-09 18:24:49 +0100

    adds adding file from command and stops running if it do not exist

--- display.rb
+++ display.rb
@@ -1,13 +1,22 @@
 require './lib/parser'
 
-file = File.open('webserver.log', 'r')
+if ARGV.empty?
+  file = File.open('webserver.log', 'r')
+else
+  if File.exist?(ARGV[0])
+    file = File.open(ARGV[0])
+  else
+    puts 'File does not exist'
+    return 0
+  end
+end
 
 splitted = []
 file.each { |line| splitted.push(line.split(' ')) }
 
 parser_object = ParserCounter.new(splitted)
 
-puts "\nSorted array of visits"
+puts "Sorted array of visits"
 print parser_object.sort_visits(parser_object.count_visits)
 
 puts "\nSorted array of unique visits"
