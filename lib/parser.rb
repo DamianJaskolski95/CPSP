e9158a532f5faf07a83f6b6401817b2b30973ca3
DamianJaskolski95 <damian.jaskolski95@gmail.com>
2020-01-09 17:55:00 +0100

    defines count_uniq_visits function

--- lib/parser.rb
+++ lib/parser.rb
@@ -10,4 +10,10 @@ class ParserCounter
     @array.each { |line| b[line.first] += 1 }
     b.to_a
   end
+
+  def count_uniq_visits
+    b = Hash.new(0)
+    @array.uniq.each { |line| b[line.first] += 1 }
+    b.to_a
+  end
 end
